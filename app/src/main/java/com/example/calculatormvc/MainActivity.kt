869e95162357ac7bf435d853d4183e619f2cceee
Dwightech <hed-jdabautista@smu.edu.ph>
2026-04-27 18:59:15 +0800

    tapos na, sorry sir kay chaguile na push iba pero naayos kona po hehe

--- app/src/main/java/com/example/calculatormvc/MainActivity.kt
+++ app/src/main/java/com/example/calculatormvc/MainActivity.kt
@@ -1,22 +1,73 @@
 package com.example.calculatormvc
 
 import android.os.Bundle
+import android.widget.*
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculatormvc.ui.theme.CalculatorMVCTheme
+import models.CalculatorModel
 
 class MainActivity : ComponentActivity() {
+    private val calcModel = CalculatorModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+       enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        //Get input from UI
+        val num1 = findViewById<EditText>(R.id.editTextNumber)
+        val num2 = findViewById<EditText>(R.id.editTextNumber2)
+        val btnAdd = findViewById<Button>(R.id.button_add)
+        val btnSubtract = findViewById<Button>(R.id.button_subtract)
+        val btnMultiply = findViewById<Button>(R.id.button_multiply)
+        val btnDivide = findViewById<Button>(R.id.button_divide)
+        val btnClear = findViewById<Button>(R.id.button_clear)
+        val result = findViewById<TextView>(R.id.textViewAnswer)
+
+        btnAdd.setOnClickListener {
+            val n1 = num1.text.toString().toDoubleOrNull() ?: 0.0
+            val n2 = num2.text.toString().toDoubleOrNull() ?: 0.0
+            calcModel.num1 = n1
+            calcModel.num2 = n2
+            val sum = n1 + n2
+            result.text = sum.toString()
+
+            //Use the Model
+            val calcResult = calcModel.add()
+            result.text = "Answer: $calcResult"
+        }
+        btnSubtract.setOnClickListener {
+            val n1 = num1.text.toString().toDoubleOrNull() ?: 0.0
+            val n2 = num2.text.toString().toDoubleOrNull() ?: 0.0
+            calcModel.num1 = n1
+            calcModel.num2 = n2
+            val calcResult = calcModel.subtract().toString()
+            result.text = "Answer: $calcResult"
+        }
+        btnMultiply.setOnClickListener {
+            val n1 = num1.text.toString().toDoubleOrNull() ?: 0.0
+            val n2 = num2.text.toString().toDoubleOrNull() ?: 0.0
+            calcModel.num1 = n1
+            calcModel.num2 = n2
+            val calcResult = calcModel.multiply()
+            result.text = "Answer: $calcResult"
+        }
+        btnDivide.setOnClickListener {
+            val n1 = num1.text.toString().toDoubleOrNull() ?: 0.0
+            val n2 = num2.text.toString().toDoubleOrNull() ?: 0.0
+            calcModel.num1 = n1
+            calcModel.num2 = n2
+            val calcResult = calcModel.divide()
+            result.text = "Answer: $calcResult"
+        }
+        btnClear.setOnClickListener {
+            num1.setText("")
+            num2.setText("")
+            result.text = "Answer: 0.0"
+        }
 //        setContent {
 //            CalculatorMVCTheme {
 //                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
